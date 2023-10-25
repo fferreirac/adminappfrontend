3e294bec6e0d85d501dce56acf430a8c1d7e0383
Francisco Ferreira <ferreiradeveloper@gmail.com>
2023-10-25 11:48:26 +0200

    ajustes en el formuilario de clientes para validaciones y llamadas al back

--- src/pages/clients/new.tsx
+++ src/pages/clients/new.tsx
@@ -1,11 +1,13 @@
 import { NextPage } from "next"
-import { Button, ButtonGroup, Card, Container, FormControl, FormErrorMessage, FormLabel, Heading, Input } from "@chakra-ui/react";
+import { Button, ButtonGroup, Card, Container, Flex, FormControl, FormErrorMessage, FormLabel, Heading, Input, Select } from "@chakra-ui/react";
 import { z }from 'zod';
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { DevTool } from "@hookform/devtools";
+import axios from "axios";
+import { env } from "~/env.mjs";
 
-const DOC_TYPES = ["RUC","NIE","Cédula","Pasaporte","CIF","NIF"] as const;
+const DOC_TYPES = ["RUC","NIE","Cédula","Pasaporte","CIF","NIF","DNI de Extrangero"] as const;
 
 // centralizar las validaciones con zod
 const schema = z.object({
@@ -13,8 +15,7 @@ const schema = z.object({
     lastname: z.string().min(3),
     email: z.string().email("Email inválido"),
     document_type: z.enum(DOC_TYPES), 
-    document_value: z.string().min(5), 
-    code: z.string().length(6, "Elcódigo debe tener 6 caracteres")
+    document_value: z.string().min(5)
 });
 
 type FieldValues = z.infer<typeof schema> //lo inferimos ya que lo de abajo no funciono
@@ -28,17 +29,41 @@ const NewClient: NextPage = () => {
     formState: {errors},
  } = useForm<FieldValues>({
     resolver: zodResolver(schema),
-    //defaultValues: {},
 });
 
-//const NewClient: NextPage = () => {
+  const onSubmit = async (data: FieldValues) => {
+    const res = await axios.post(`${env.NEXT_PUBLIC_BACKEND_BASE_URL}/clients`, data, {withCredentials:true});
+    console.log({res});
+  }
+
   return (
     <Container marginTop={8}>
         <Card padding={4}>
             <Heading textAlign={"center"} marginBottom={6}>Nuevo Cliente</Heading>            
         
-            <form action="">
-                <FormControl marginBottom={5} isInvalid={!!errors.email} /*esto es para que funciones el menjaje de erorr */>
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+                <FormControl marginBottom={5} isInvalid={!!errors.firstname}>
+                    <FormLabel>Nombre</FormLabel>
+                        <Input 
+                            type='text' 
+                            placeholder="nombre"
+                            {...register("firstname")}
+                        />
+                    <FormErrorMessage>{errors.firstname?.message}</FormErrorMessage>
+                </FormControl>
+
+                <FormControl marginBottom={5} isInvalid={!!errors.lastname}>
+                    <FormLabel>Apellido</FormLabel>
+                        <Input 
+                            type='text' 
+                            placeholder="apellido"
+                            {...register("lastname")}
+                        />
+                    <FormErrorMessage>{errors.lastname?.message}</FormErrorMessage>
+                </FormControl>
+
+                <FormControl marginBottom={5} isInvalid={!!errors.email} >
                     <FormLabel>Email</FormLabel>
                         <Input 
                             type='email' 
@@ -48,7 +73,26 @@ const NewClient: NextPage = () => {
                     <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
                 </FormControl>
 
-                <Button colorScheme="purple" onClick={ ()=> {}}>
+                <Flex>    
+                    <FormControl flex={4} marginRight={3}>
+                        <FormLabel>Tipo de Documento</FormLabel>
+                        <Select placeholder="Seleccionar" {...register("document_type")}>
+                            {DOC_TYPES.map(dt => (
+                                <option key={dt} value={dt}>{dt}</option>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    <FormControl marginBottom={5} isInvalid={!!errors.document_value} flex={4}>
+                        <FormLabel>Documento</FormLabel>
+                            <Input 
+                                type='text' 
+                                placeholder="tu documento"
+                                {...register("document_value")}
+                            />
+                        <FormErrorMessage>{errors.document_value?.message}</FormErrorMessage>
+                    </FormControl>
+                </Flex>
+                <Button colorScheme="purple" type="submit">
                     Create
                 </Button>
             </form>        
